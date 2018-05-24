b907164c4318274640d26fea0cccf3259eefad95
Dawn Tossie <dawn.tossie@gmail.com>
2018-05-24 00:29:03 -0400

    app listening

---
+++ app/data/friends.js
@@ -0,0 +1,13 @@
+//DATA
+// Below data will hold all of the friends tables.
+
+var friendArray = [
+  {
+    name: "",
+    photo: "",
+    scores: []
+  }
+];
+
+// Note how we export the array. This makes it accessible to other files using require.
+module.exports = friendArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,33 @@
+// LOAD DATA
+// We are linking our routes to a series of "data" sources.
+// These data sources hold arrays of information 
+
+var friendsData = require("../data/friends");
+
+// ROUTING
+
+module.exports = function(app) {
+  // API GET Requests
+  // Below code handles when users "visit" a page.
+  // In each of the below cases when a user visits a link
+  // (ex: localhost:PORT/api/admin... they are shown a JSON of the data in the table)
+  // ---------------------------------------------------------------------------
+
+  app.get("/api/friends", function(req, res) {
+    res.json(friendsData);
+  });
+
+  app.post("/api/friends", function(req, res) {
+    // Note the code here. Our "server" will respond to requests and let users know if they have a table or not.
+    // It will do this by sending out the value "true" have a table
+    // req.body is available since we're using the body-parser middleware
+    if (friendsData.length < 5) {
+      tableData.push(req.body);
+      res.json(true);
+    }
+    else {
+      waitListData.push(req.body);
+      res.json(false);
+    }
+  });
+};
