65af3e6b4b3601e4967a6233d37f0404de52fd8a
Dawn Tossie <dawn.tossie@gmail.com>
2018-07-11 13:37:23 -0400

    added friend data

--- app/data/friends.js
+++ app/data/friends.js
@@ -3,9 +3,100 @@
 
 var friendArray = [
   {
-    name: "",
-    photo: "",
-    scores: []
+    name: "Amanda Leach",
+    photo: "../images/photo-1477118476589-bff2c5c4cfbb.jpg",
+    scores: [
+      "5",
+      "4",
+      "3",
+      "3",
+      "1",
+      "1",
+      "2",
+      "3",
+      "2",
+      "1"
+    ]
+  },
+  {
+    name: "Ethan Rowe",
+    photo: "../images/photo-1500048993953-d23a436266cf.jpg",
+    scores: [
+      "1",
+      "1",
+      "5",
+      "3",
+      "4",
+      "2",
+      "5",
+      "5",
+      "2",
+      "3"
+    ]
+  },
+  {
+    name: "Sarah Anison",
+    photo: "../images/photo-1477524076598-003de1c88dff.jpg",
+    scores: [
+      "4",
+      "1",
+      "2",
+      "4",
+      "5",
+      "2",
+      "1",
+      "4",
+      "4",
+      "4"
+    ]
+  },
+  {
+    name: "Jay Thomas",
+    photo: "../images/photo-1494757126156-ca5d7c7bb775.jpg",
+    scores: [
+      "3",
+      "3",
+      "1",
+      "2",
+      "1",
+      "2",
+      "5",
+      "4",
+      "1",
+      "1"
+    ]
+  },
+  {
+    name: "Melissa Flowers",
+    photo: "../images/photo-1506726447732-9090b8586f05",
+    scores: [
+      "2",
+      "2",
+      "4",
+      "1",
+      "3",
+      "4",
+      "4",
+      "5",
+      "5",
+      "5"
+    ]
+  },
+  {
+    name: "Carl Lee",
+    photo: "../images/photo-1520248033529-d05cd1981e6f",
+    scores: [
+      "3",
+      "5",
+      "5",
+      "3",
+      "2",
+      "2",
+      "1",
+      "4",
+      "3",
+      "1"
+    ]
   }
 ];
 
